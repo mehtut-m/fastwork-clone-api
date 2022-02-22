baf17114df31f3972b3dd526713cb665701093d9
mehtut-m <mehtut.m@gmail.com>
2022-02-22 11:10:11 +0700

    add getsubcat

--- routes/categoriesRoute.js
+++ routes/categoriesRoute.js
@@ -1,8 +1,12 @@
-const express = require("express");
-const { getAllCategories } = require("../controllers/categoriesController");
+const express = require('express');
+const {
+  getAllCategories,
+  getSubCategoryById,
+} = require('../controllers/categoriesController');
 
 const router = express.Router();
 
-router.get("/", getAllCategories);
+router.get('/', getAllCategories);
+router.get('/sub-category/:id', getSubCategoryById);
 
 module.exports = router;
