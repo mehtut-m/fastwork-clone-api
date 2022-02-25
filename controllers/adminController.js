b6f1b7022e74322c03ada8a907daa3cbe7f13efe
Phaithun-Ton <phaithun.ap@gmail.com>
2022-02-25 10:32:16 +0700

    'addmin-reject'

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -18,7 +18,13 @@ exports.adminApprove = async (req, res, next) => {
       return res.status(400).json({ message: "user not found" });
     }
 
-    const freelance = await FreelanceInfo.findOne({ where: { userId } });
+    const freelance = await FreelanceInfo.findOne({
+      where: { userId, status: "SUBMIT" },
+    });
+
+    if (!freelance) {
+      return res.status(400).json({ message: "freelance not found" });
+    }
 
     if (freelance.status === "APPROVE") {
       return res
@@ -26,15 +32,11 @@ exports.adminApprove = async (req, res, next) => {
         .json({ message: "this freelance status is already approve" });
     }
 
-    if (!freelance) {
-      return res.status(400).json({ message: "freelance not found" });
-    }
-
     freelance.update({
       status: "APPROVE",
     });
 
-    res.status(200).json({ message: "success" });
+    res.status(200).json({ message: "approve freelance success" });
   } catch (err) {
     next(err);
   }
@@ -43,6 +45,33 @@ exports.adminApprove = async (req, res, next) => {
 // TODO: Admin reject
 exports.adminReject = async (req, res, next) => {
   try {
+    const { userId } = req.params;
+    if (req.user.role !== "ADMIN") {
+      return res.status(403).json({ message: "you do not have permission" });
+    }
+
+    if (typeof userId !== "string" || userId.trim() === "") {
+      return res.status(400).json({ message: "user id is require" });
+    }
+
+    const user = await User.findOne({ where: { id: userId } });
+    if (!user) {
+      return res.status(400).json({ message: "user not found" });
+    }
+
+    const freelance = await FreelanceInfo.findOne({
+      where: { userId, status: "SUBMIT" },
+    });
+
+    if (!freelance) {
+      return res.status(400).json({ message: "freelance not found" });
+    }
+
+    freelance.update({
+      status: "REJECT",
+    });
+
+    res.status(200).json({ message: "reject freelance success" });
   } catch (err) {
     next(err);
   }
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const authenticate = require("../middlewares/authenticate");
-const { adminApprove } = require("../controllers/adminController");
+const { adminApprove, adminReject } = require("../controllers/adminController");
 
 const router = express.Router();
 
+// TODO: Admin approve
 router.patch("/approve/:userId", authenticate, adminApprove);
+
+// TODO: Admin reject
+router.patch("/reject/:userId", authenticate, adminReject);
 
 module.exports = router;
